37e641e218f8a40e228a2d59ae1a6e9e0520d689
Graham McGregor <graham_mcgregor@trendmicro.com>
2019-04-18 12:01:16 -0400

    Fix parsing test

--- src/test/java/com/trendmicro/deepsecurity/smartcheck/SmartCheckActionTest.java
+++ src/test/java/com/trendmicro/deepsecurity/smartcheck/SmartCheckActionTest.java
@@ -3,6 +3,8 @@ package com.trendmicro.deepsecurity.smartcheck;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.apache.commons.io.FilenameUtils;
 import org.junit.Test;
@@ -10,12 +12,12 @@ import org.junit.Test;
 public class SmartCheckActionTest {
 
 	@Test
-	public void testAction() throws Exception {
+	public void testParseScan() throws Exception {
 		String fixturePath = new File("src/test/resources/scan-result-sample.json").toString();
+		String contents = new String(Files.readAllBytes(Paths.get("src/test/resources/scan-result-sample.json")));
 		String dirName = FilenameUtils.getFullPath(fixturePath);
-		String fileName = FilenameUtils.getName(fixturePath);
 
-		SmartCheckAction action = new SmartCheckAction(dirName, fileName, "0.0.0.0:8443");
+		SmartCheckAction action = new SmartCheckAction(dirName, contents, "0.0.0.0:8443");
 		// if not null then we can assume that the reading and parsing happened ok.
 		assertNotNull(action.getScanResult());
 	}
